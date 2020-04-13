c22aba9f569b973ed0f9728383f00dcf789fa86a
Austin Santoso <santoso.austin@gmail.com>
2020-04-14 02:55:37 +0800

    april14 script

---
+++ april14_runme_tembusu/utilVisualisation.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def visualize(history, filename_extension=""):
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(1, len(acc)+1)
+
+    columns=['epoch',
+            'accuracy',
+            'val_accuracy',
+            'loss',
+            'val_loss']
+    df = pd.DataFrame(list(zip(epochs, acc, val_acc, loss, val_loss)), columns=columns)
+    df.to_csv ('history' + filename_extension + '.csv', index = False, header=True)
+
+    plt.figure(figsize=(10,10))
+
+    plt.subplot(2,1,1)
+
+    plt.plot(epochs, acc, 'b', label='Training accuracy')
+    plt.plot(epochs, val_acc, 'r', label='Validation accuracy')
+    plt.title('Training and Validation Accuracy')
+    plt.legend()
+
+    plt.subplot(2,1,2)
+
+    plt.plot(epochs, loss, 'b', label='Training loss')
+    plt.plot(epochs, val_loss, 'r', label='Validation loss')
+    plt.title('Training and Validation Loss')
+    plt.legend()
+
+    plt.savefig('acurracy_loss' + filename_extension + '.png')
